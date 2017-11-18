97272ee781ac105eea52bd5fb49f0b7ee83c2e3e
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2017-11-18 18:09:20 +0100

    TO DO : création album (API) + add destriction title on picture

--- screens/MyPictures.js
+++ screens/MyPictures.js
@@ -9,9 +9,10 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View, AsyncStorage, Button,FlatList
+    View, AsyncStorage, Button,FlatList, Image, WebView, ScrollView, SectionList
 } from 'react-native';
 
+import QRCode from 'react-native-qrcode';
 
 
 export default class Home extends Component<{}> {
@@ -75,6 +76,7 @@ export default class Home extends Component<{}> {
                         //TODO: Une fois ca fait proposer pour chacune un QRCODE (npm install) qui prendre le name image + URL (API a crééer une route pour display l'image selon son name) sur un clik
 
                         this.setState({
+                            json_pictures: json,
                             pictures: arrayPictures,
                         });
                         console.log(this.state.pictures);
@@ -106,11 +108,40 @@ export default class Home extends Component<{}> {
             );
         }else if(this.state.pictures !== null && this.state.displayPictures === true){
             //TODO listView of pictures avec leur element + porposer un QRcode en fonction du nom + URL (localhost) (a faire coté api pour display)
+            //TODO on .htaccess give access public on web/upload/pictures directory
+            //TODO add a loop on array (start to one) and for each picture of user, display corresponding images
+            //todo          <Image style={{width: 50, height: 50}}source={{uri: 'http://10.0.2.2/albumzAPI/public/upload/pictures/test.jpg'}}/>
+            console.log("LENGTH", this.state.pictures.length)
+            //console.log("flat_list_array(json)", this.state.json_pictures);
+            console.log("flat_list_json", this.state.json_pictures[0].name);
+
             return(
-                <Text>
-                    {this.state.pictures}
-                </Text>
-            );
+            <ScrollView>
+                <FlatList
+                    data={this.state.json_pictures}
+                    renderItem={
+                        ({item}) =>
+                        <View>
+                            <Text>{item.date_publication}</Text>
+                            <Image style={{width: 150, height: 150}}
+                                   source={{uri:`http://10.0.2.2/albumzAPI/public/upload/pictures/${item.name}`}}/>
+                            <Text>{"\n"}</Text>
+                            <Text>Share picture</Text>
+                            <Text>{"\n"}</Text>
+                            <QRCode
+                                value={`http://localhost:8000/albumzAPI/public/upload/pictures/${item.name}`}
+                                size={200}
+                                bgColor='purple'
+                                fgColor='white'/>
+                            <View>
+                                <Text>______________________________________</Text>
+                            </View>
+                            <Text>{"\n"}</Text>
+                        </View>
+                    }
+                />
+            </ScrollView>
+                );
         } else{
             return (
                 <View style={styles.container}>
